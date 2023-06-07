5c2a739d2d16d3a278688ddc53ea8f8272ac0a4e
NHNerd <promenader.off@gmail.com>
2023-06-07 17:59:22 +0400

    - PWA...

--- service-worker.js
+++ service-worker.js
@@ -18,22 +18,25 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(async () => {
-    const cache = await caches.open(CACHE_NAME);
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
 
-    // match the request to our cache
-    const cachedResponse = await cache.match(event.request);
+      // match the request to our cache
+      const cachedResponse = await cache.match(event.request);
 
-    // check if we got a valid response
-    if (cachedResponse !== undefined) {
-      // Cache hit, return the resource
-      return cachedResponse;
-    } else {
-      // Otherwise, go to the network
-      return fetch(event.request);
-    }
-  });
+      // check if we got a valid response
+      if (cachedResponse !== undefined) {
+        // Cache hit, return the resource
+        return cachedResponse;
+      } else {
+        // Otherwise, go to the network
+        return fetch(event.request);
+      }
+    })()
+  );
 });
+
 // '/',
 // 'index.html',
 // 'index.css',
